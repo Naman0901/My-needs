68370554b2f5f50342c63fe7941785a9231c806d
Naman0901 <73291949+Naman0901@users.noreply.github.com>
2021-04-21 15:46:10 +0530

    Update audio download.py

--- audio download.py
+++ audio download.py
@@ -4,9 +4,9 @@ try:
     youtube_video_url=input()
     yt_obj = YouTube(youtube_video_url) 
     stream=yt_obj.streams.filter(type="audio")
-    '''for i in range(len(stream)):
-        'print(i ,stream[i])
-    'n=int(input("Enter The Corresponding Number You Want To Download(-1 To Exit):"))'''
+    for i in range(len(stream)):
+        print(i ,stream[i])
+    n=int(input("Enter The Corresponding Number You Want To Download(-1 To Exit):"))
     stream[0].download(output_path='D:/NAMAN/Satsang/ભક્તચિંતામણિ', filename=yt_obj.title)
     print('Video Downloaded Successfully')
 except Exception as e:
